e6a37f634f020161049c688e61bc230f29477327
anna-ermakova <korovanna8@mail.ru>
2023-07-20 20:45:12 +0300

    ДЗ по теме Логирование и конфигурирование готово. Добавлено логирование в методы сервисов Аватар и Факультет

--- src/main/java/ru/hogwarts/school/services/FacultyService.java
+++ src/main/java/ru/hogwarts/school/services/FacultyService.java
@@ -1,5 +1,6 @@
 package ru.hogwarts.school.services;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.entity.Faculty;
 import ru.hogwarts.school.entity.Student;
@@ -7,10 +8,12 @@ import ru.hogwarts.school.repositorys.FacultyRepository;
 import ru.hogwarts.school.repositorys.StudentRepository;
 
 import java.util.Collection;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @Service
 public class FacultyService {
+    private static final Logger LOG = (Logger) LoggerFactory.getLogger(FacultyService.class);
 
     private final FacultyRepository facultyRepository;
 
@@ -22,34 +25,42 @@ public class FacultyService {
     }
 
     public Faculty addFaculty(Faculty faculty) {
+        LOG.info("Was invoked method create");
         return facultyRepository.save(faculty);
     }
 
     public Faculty getFaculty(long id) {
+        LOG.info("Was invoked method getFaculty");
         return facultyRepository.findById(id).orElse(null);
     }
 
     public Faculty editFaculty(Faculty faculty) {
+        LOG.info("Was invoked method editFaculty");
         return facultyRepository.save(faculty);
     }
 
     public void removeFaculty(long id) {
+        LOG.info("Was invoked method removeFaculty");
         facultyRepository.deleteById(id);
     }
 
     public Collection<Faculty> getFacultiesByColor(String color) {
+        LOG.info("Was invoked method getFacultiesByColor");
         return facultyRepository.findByColorLike(color);
     }
 
     public Collection<Faculty> getAll() {
+        LOG.info("Was invoked method getAll");
         return facultyRepository.findAll();
     }
 
     public Collection<Faculty> getFacultiesByColorOrName(String param) {
+        LOG.info("Was invoked method getFacultiesByColorOrName");
         return facultyRepository.findAllByColorContainingIgnoreCaseOrNameContainingIgnoreCase(param, param);
     }
 
     public Collection<Student> getStudents(Long facultyId) {
+        LOG.info("Was invoked method getStudents");
         return studentRepository.findAll().stream()
                 .filter(s -> s.getFaculty().getId() == facultyId)
                 .collect(Collectors.toList());
